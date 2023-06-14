6061ce09f9ca91cfda0807331c36041b2b7bfcf0
Frosa <127599287+GeneralFrosa@users.noreply.github.com>
2023-06-14 19:56:41 +0700

    Try to build pbrp

--- omni_RMX3371.mk
+++ omni_RMX3371.mk
@@ -11,13 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from RMX3371 device
 $(call inherit-product, device/realme/RMX3371/device.mk)
 
 PRODUCT_DEVICE := RMX3371
-PRODUCT_NAME := twrp_RMX3371
+PRODUCT_NAME := omni_RMX3371
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := GT Neo 3T
 PRODUCT_MANUFACTURER := realme
