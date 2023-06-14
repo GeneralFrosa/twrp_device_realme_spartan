6b8c2e61ba133280024f4fcc67d954c75e457f40
Frosa <127599287+GeneralFrosa@users.noreply.github.com>
2023-06-14 20:08:52 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_RMX3371.mk
+    $(LOCAL_DIR)/omni_RMX3371.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_RMX3371-user \
-    twrp_RMX3371-userdebug \
-    twrp_RMX3371-eng
+    omni_RMX3371-user \
+    omni_RMX3371-userdebug \
+    omni_RMX3371-eng
