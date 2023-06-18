484479f96c25c069a405be08ff22fb4d82416f6c
GeneralFrosa <sennafarras98@gmail.com>
2023-06-18 10:18:26 +0700

    RMX3371 : Add Unified Scripts

---
+++ recovery/root/system/bin/unified-script.sh
@@ -0,0 +1,42 @@
+#!/system/bin/sh
+# This script is needed to automatically set device props.
+
+load_RMX3371IN()
+{
+      echo "Setting up props for RXM3371IN"
+      resetprop "ro.build.product" "RE54E4L1"
+      resetprop "ro.product.model" "RMX3371"
+      resetprop "ro.product.device" "RE54E4L1"
+      resetprop "ro.product.product.device" "RE54E4L1"
+      resetprop "ro.commonsoft.ota" "RE879AL1"
+      resetprop "ro.separate.soft" "21732"
+}
+
+load_RMX3371ID/EU()
+{
+      echo "Setting up props for RMX3371ID/EU"
+      resetprop "ro.build.product" "RE54E4L1"
+      resetprop "ro.product.model" "RMX3371"
+      resetprop "ro.product.device" "RE54E4L1"
+      resetprop "ro.product.product.device" "RE54E4L1"
+      resetprop "ro.commonsoft.ota" "RE879AL1"
+      resetprop "ro.separate.soft" "21733"
+}
+
+project=$(cat /proc/oplusVersion/prjName)
+echo $project
+
+case $project in
+     "2169A")
+         load_RMX3371IN
+              ;;
+     "2169B")
+          load_RMX3371ID/EU
+              ;;
+           *)
+          load_RMX3371IN
+              ;;
+
+esac
+
+exit 0
